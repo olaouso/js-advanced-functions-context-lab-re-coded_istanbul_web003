d8082b19e29231f3a611846ec265732caf24beb2
Ola Ouso <ouso.ola@gmail.com>
2020-10-24 19:19:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
-/* Your Code Here */
+function createEmployeeRecord([firstName, familyName,title,payPerHour]){
+    const employeeObj = {
+        firstName: firstName,
+        familyName:familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents:[]
+    }
+    return employeeObj;
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployeeRecords(arrsOfEmployees){
+    const objsOfEmployees = arrsOfEmployees.map(
+        employeeArr=>createEmployeeRecord(employeeArr)
+    );
+    return objsOfEmployees;
+}
+const date= "YYYY-MM-DD 1100";
+function createTimeInEvent(dateStr){
+    const dateArr= dateStr.split(" ");
+    const hour= parseInt(dateArr[1]);
+    const date= dateArr[0];
+    const timeInfo= {
+        type: "TimeIn",
+        hour: hour,
+        date: date
+    }
+    this.timeInEvents.push(timeInfo);
+    return this; //obj which we apply the function
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createTimeOutEvent(dateStr){
+    const dateArr= dateStr.split(" ");
+    const hour= parseInt(dateArr[1]);
+    const date= dateArr[0];
+    const timeInfo= {
+        type: "TimeOut",
+        hour: hour,
+        date: date
+    }
+    this.timeOutEvents.push(timeInfo);
+    return this;
+}
 
+function hoursWorkedOnDate(date){
+    const dayStart= this.timeInEvents.find(timeInfo=> timeInfo.date===date);
+    const dayEnd= this.timeOutEvents.find(timeInfo=> timeInfo.date===date);
+    const start = dayStart.hour;
+    const end = dayEnd.hour;
+    const hourWorked= (end - start)/100; //hour = hhmm 2h=200
+    return hourWorked;
+}
+
+function wagesEarnedOnDate(date){
+    const dailyhours= hoursWorkedOnDate.call(this,date); // call to set the context to the obj we apply the function
+    const payOwned= dailyhours*this.payPerHour;
+    return payOwned;
+}
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -20,3 +68,14 @@ let allWagesFor = function () {
 
     return payable
 }
+function findEmployeeByFirstName(srcArray, firstName){
+    for (const person of srcArray){
+        person.firstName===firstName;
+        return person;//return only the first match
+    }
+     return undefined;
+}
+
+function calculatePayroll(array){
+    const total = array.reduce((acc, curr)=> acc+=allWagesFor.call(curr),0);
+    return total;}
